f31353d4c0981c8b6dc11774ea09bbe14ee8ad9b
mounikay123 <48583059+mounikay123@users.noreply.github.com>
2019-08-01 03:26:17 -0700

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const Header = () => (
+    <header>
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <a className="navbar-item" href="/">
+               
+                    <h3>Header</h3>
+                </a>
+            </div>
+        </nav>
+    </header>
+)
+
+export default Header;
+
+
+
+
